d37b08d78326d97dd16bc3ce2d0890e405d440e4
dfmn143 <5310110530@email.psu.ac.th>
2019-12-11 15:05:43 +0700

    firebase contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Observable } from "rxjs";
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
+  nameValue = "";
+  emailValue = "";
+  textValue = "";
+  contact: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public db: AngularFireDatabase) {
+    this.contact = db.list("contact").valueChanges();
+  }
+  onSubmit() {
+    this.db
+      .list("contact")
+      .push({
+        name: this.nameValue,
+        email: this.emailValue,
+        comment: this.textValue
+      });
+    this.nameValue = "";
+    this.emailValue = "";
+    this.textValue = "";
   }
 
+  ngOnInit() {}
 }
