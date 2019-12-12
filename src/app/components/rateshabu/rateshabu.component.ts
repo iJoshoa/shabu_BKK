a922765789529da178bc013b2dda9dad2b3aef03
dfmn143 <5310110530@email.psu.ac.th>
2019-12-12 20:12:40 +0700

    sourt

--- src/app/components/rateshabu/rateshabu.component.ts
+++ src/app/components/rateshabu/rateshabu.component.ts
@@ -1,24 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Query } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/database";
 import { Observable, timer } from "rxjs";
 
 @Component({
-  selector: 'app-rateshabu',
-  templateUrl: './rateshabu.component.html',
-  styleUrls: ['./rateshabu.component.css']
+  selector: "app-rateshabu",
+  templateUrl: "./rateshabu.component.html",
+  styleUrls: ["./rateshabu.component.css"]
 })
 export class RateshabuComponent implements OnInit {
   one = "";
   two = "";
-  items :Observable<any[]>
-  constructor(public db : AngularFireDatabase) { 
-    this.items = db.list('shabushop').valueChanges();
+  items: Observable<any[]>;
+  constructor(public db: AngularFireDatabase) {
+    this.items = db.list("shabushop",ref => ref.orderByChild('s_price')).valueChanges();
   }
-  onSubmit() {
-   console.log(this.one+ " " +this.two);
-
-  }
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
